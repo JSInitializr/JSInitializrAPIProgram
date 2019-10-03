a987dd92f18e0eb0904d531fb4a409089d03abfb
sachin thakur <sachin26thakur@gmail.com>
2019-10-03 20:11:16 +0530

    Handle request and added logic to copy folder to destionation and write json file with updated content

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,59 +1,127 @@
 var zip = require('express-easy-zip');
 var fs = require('fs');
 var path = require('path');
+var ncp = require('ncp').ncp;
+
 
 
 exports.create = function (request, response, next) {
     try {
         var params = request.body;
-        var foldername;
-        switch (params.category) {
-            case 'angular':
-                foldername = 'angular'
-                break;
-            case 'react':
-                foldername = params.type === 'js' ? 'reactJs' : 'reactTS';
-                break;
-            case 'node':
-                foldername = params.type === 'js' ? 'nodeJavascript' : 'nodeTypescript';
-                break;
-            case 'reactNative':
-                foldername = 'reactNative';
-                break;
-        }
-        if (!foldername) return response.send({ status: 0, message: 'Oops something went wrong!!!' });
-        var projectListPath = __dirname + './../public/';
-        fs.readdir(projectListPath, (error, list) => {
-            if (error) {
-                console.log(error);
-                return response.send(error);
+        const sourceFolderPath = getSourceFolderPath(params);
+        const destinationFolderPath = getDestinationFolderPath();
+
+        return copyFolder(sourceFolderPath, destinationFolderPath, (err) => {
+            if (err) {
+                return err;
             }
-            return response.zip({
-                files: [{
-                    content: 'this is a string',
-                    name: 'file-name',
-                    mode: 0755,
-                    comment: 'comment-for-the-file',
-                    date: new Date(),
-                    type: 'file'
-                },
-                { path: path.join(projectListPath, foldername), name: foldername }
-                ],
-                filename: foldername + '.zip'
-            });
-        })
+            const packageJsonFilePath = path.join(destinationFolderPath, 'package.json')
+            saveToPackage(packageJsonFilePath,'dependency',params.dependenciesItem);
+            return createZip(destinationFolderPath,response);
+        });
+
+
     } catch (exception) {
         console.log(exception)
         return response.send(exception);
     }
 }
 
-function getFolderName (category, type) {
+
+function saveToPackage(packageJsonFilePath, category, requestedData) {
+
+    const data = readFromFile(packageJsonFilePath);
+    let json;
+
     try {
-        let foldername;
-        
-    } catch (exception) {
-        console.log(exception)
-        return '';
+        json = JSON.parse(data);
+    } catch (e) {
+        console.error('Could not parse package.json. Stop.');
+    }
+
+    let modifiedJsonData = Object.assign(json[getTargetName(category)] || {}, requestedData)
+
+    modifiedJsonData = Object.keys(modifiedJsonData).sort().reduce((res, key) => {
+        res[key] = modifiedJsonData[key];
+        return res;
+    }, {});
+
+    json[getTargetName(category)] = modifiedJsonData;
+
+    writeToFile(packageJsonFilePath, JSON.stringify(json, null, 2));
+    console.log('Done.');
+}
+
+
+
+function readFromFile(filePath) {
+    const readFileData = fs.readFileSync(filePath);
+    return readFileData.toString();
+}
+
+function writeToFile(filePath, fileContent) {
+    fs.writeFileSync(filePath, fileContent);
+};
+
+
+function getTargetName(target) {
+    switch (target) {
+        case 'dev': return 'devDependencies';
+        case 'peer': return 'peerDependencies';
+        case 'bundled': return 'bundledDependencies';
+        case 'optional': return 'optionalDependencies';
+        default: return 'dependencies';
     }
 }
+
+const getSourceFolderPath = (params) => {
+
+    const language = params.tabs.find(item => {
+        return item.label === 'Language';
+    })
+    const technology = params.tabs.find(item => {
+        return item.label === 'Technology';
+    });
+
+    const folderName = language.value + '_' + technology.value;
+    var projectDirPath = path.resolve(__dirname + './../public/');
+
+    const folderSourcePath = path.join(projectDirPath, folderName)
+    return folderSourcePath;
+}
+
+
+const getDestinationFolderPath = () => {
+    return path.resolve(__dirname + './../public/TempFolder');
+}
+
+const copyFolder = (source, destination, callback) => {
+    ncp.limit = 16;
+
+    ncp(source, destination, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('done!');
+        callback(err);
+    });
+}
+
+const createZip = (folderPath,response) => {
+        return response.zip({
+            files: [{
+                content: 'this is a string',
+                name: 'file-name',
+                mode: 0755,
+                comment: 'comment-for-the-file',
+                date: new Date(),
+                type: 'file'
+            },
+            { path: folderPath, name: "foldername" }
+            ],
+            filename: "foldername" + '.zip'
+        });
+}
+
+
+
---
+++ routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var zip = require('express-easy-zip');
+var fs = require('fs');
+var path = require('path');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var projectListPath = __dirname + './../public/';
+  fs.readdir(path.resolve(projectListPath), (error, list) => {
+    if(error) {
+      console.log('filepath', error);
+      res.send(error);
+      return;
+    } 
+    res.zip({
+      files: [{
+        content: 'this is a string',      //options can refer to [http://archiverjs.com/zip-stream/ZipStream.html#entry](http://archiverjs.com/zip-stream/ZipStream.html#entry)
+        name: 'file-name',
+        mode: 0755,
+        comment: 'comment-for-the-file',
+        date: new Date(),
+        type: 'file'},
+        { path: path.join(projectListPath, 'nodeJavascript'), name: 'nodeJavascript'}
+      ],
+      filename: 'abc.zip'
+    })
+  })
+});
+
+module.exports = router;
